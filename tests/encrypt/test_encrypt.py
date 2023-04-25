55cf12d84b111165cd88699cd6ca1a111440be99
Helton-Pereira <helton.bfsm@gmail.com>
2023-04-25 15:47:18 -0300

    Testes para função encrypt_message (req 02)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(
+        TypeError, match="tipo inválido para message"
+    ):
+        encrypt_message(1, 2)
+
+    with pytest.raises(
+        TypeError, match="tipo inválido para key"
+    ):
+        encrypt_message("teste", "x")
+
+    assert encrypt_message("teste", 8) == "etset"
+
+    assert encrypt_message("teste", 2) == "ets_et"
+
+    assert encrypt_message("teste", 3) == "set_et"
