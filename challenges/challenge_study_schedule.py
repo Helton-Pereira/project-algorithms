d442a9adbdfa23dc3f0b2a7eb598a13580a18f2f
Helton-Pereira <helton.bfsm@gmail.com>
2023-04-25 15:08:48 -0300

    Req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    ...
+    result = 0
+    if type(target_time) is not int:
+        return None
+    for i, j in permanence_period:
+        if type(i) is not int or type(j) is not int:
+            return None
+        elif i <= target_time <= j:
+            result += 1
+    return result
